07f8a86d7a3bdbc0a9b347bbe20909c07c170e26
Hamutahl Cohen <43823923+hamutahlc@users.noreply.github.com>
2021-08-31 12:37:33 -0700

    libraries added

--- dataPrep/libraries.R
+++ dataPrep/libraries.R
@@ -10,3 +10,9 @@ library(piecewiseSEM)
 library(lme4)
 library(lmerTest)
 library(brms)
+
+install.packages("R2admb")
+install.packages("glmmADMB",
+                 repos=c("http://glmmadmb.r-forge.r-project.org/repos",
+                         getOption("repos")),
+                 type="source")
